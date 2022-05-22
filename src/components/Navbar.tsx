1109ed3b00da99052d8a7d705ff0c0aca399e3e5
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-05-22 15:53:18 -0300

    feat: Navbar component responsive layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,66 @@
+import { styled, AppBar, Box, Toolbar, Typography, InputBase, Badge, Avatar } from "@mui/material";
+import { Mail, Notifications, Pets } from "@mui/icons-material";
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between"
+});
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  width: "40%",
+  borderRadius: theme.shape.borderRadius
+}));
+
+const Icons = styled(Box)(({ theme }) =>({
+  display: "none",
+  alignItems: "center",
+  gap: "20px",
+  [theme.breakpoints.up("sm")]:{
+    display: "flex"
+  }
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  }
+}));
+
 export function Navbar() {
   return (
-    <div>Navbar</div>
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography sx={{ display: { xs: "none", sm: "block" } }} variant="h6">
+          Social Network
+        </Typography>
+        <Pets sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={4} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          />
+        </Icons>
+        <UserBox>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          />
+        </UserBox>
+      </StyledToolbar>
+    </AppBar>
   );
 }
